4f66d861c3a831ec20efcdc9603e1cbaa41d5054
tauksun <tauksun@gmail.com>
2022-10-12 00:39:11 +0530

    added proxy configuration to develop on local connecting frontend & backend

--- frontend/src/react-constants.ts
+++ frontend/src/react-constants.ts
@@ -1,28 +1,15 @@
 const environmentVariables = process.env;
 
-// Check if running with docker, variable set by script //
-const docker_run = environmentVariables.REACT_APP_DOCKER_BUILD;
-
 /////////////////////////////
 // Get PORT Configuration //
 ////////////////////////////
 
 // Get ports, set by .env
-const docker_port_in_env = environmentVariables.REACT_APP_DOCKER_PORT
-  ? parseInt(environmentVariables.REACT_APP_DOCKER_PORT)
-  : null;
-
-// Get default fallback docker port
-const default_fallback_docker_port =
-  environmentVariables.REACT_APP_DOCKER_FALLBACK_PORT
-    ? parseInt(environmentVariables.REACT_APP_DOCKER_FALLBACK_PORT)
-    : null;
-
 const backend_port_in_env = environmentVariables.REACT_APP_BACKEND_PORT
   ? parseInt(environmentVariables.REACT_APP_BACKEND_PORT)
   : null;
 
-// Get default backend port, set by script when run on local environment
+// Get default fallback backend port, set by script when run on local environment
 const default_fallback_backend_port =
   environmentVariables.REACT_APP_DEFAULT_FALLBACK_BACKEND_PORT
     ? parseInt(environmentVariables.REACT_APP_DEFAULT_FALLBACK_BACKEND_PORT)
@@ -41,15 +28,11 @@ const default_fallback_backend_host =
 // Configure //
 ///////////////
 
-let backendHost: string | null =
+const backendHost: string | null =
   backend_host_in_env || default_fallback_backend_host;
 
-let backendPort: number | null;
-if (docker_run) {
-  backendPort = docker_port_in_env || default_fallback_docker_port;
-} else {
-  backendPort = backend_port_in_env || default_fallback_backend_port;
-}
+const backendPort: number | null =
+  backend_port_in_env || default_fallback_backend_port;
 
 ////////////////////////////////////////////
 //~~~~~~~~~~~~~~ Constants ~~~~~~~~~~~~~~~//
